1a836ccb058acff72b4c60de14ace2515fbd8509
Sidney Mau <sidneymau@gmail.com>
2026-01-31 16:55:35 -0500

    commit scripts in bin

---
+++ install.sh
@@ -0,0 +1,21 @@
+#/usr/bin/env bash
+
+__bin=$HOME/bin
+
+scripts=(
+	journal
+	notes
+)
+
+for script in "${scripts[@]}"; do
+	if [[ -L $__bin/$script ]]; then
+		# echo "skipping $script"
+		target=$(readlink -fv $script)
+		printf "'%s' is '%s'\n" "$__bin/$script" "$target"
+	else
+		if [[ -d $__bin/$script && ! -L $__bin/$script ]]; then
+			rm -irv "$__bin/$script"
+		fi
+		ln -sv "$PWD/$script" "$__bin/$script"
+	fi
+done
